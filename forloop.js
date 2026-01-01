50e7b62bf97ad30f8fb54ea9b6563c6279ab1d31
mirzamehreen <mirzamehreen@gmail.com>
2026-01-01 11:32:51 -0800

    first comit

---
+++ forloop.js
@@ -0,0 +1,37 @@
+
+for(i=1; i<=10; i ++){
+    console.log(i);
+    
+}
+
+for(i=10; i>=1; i--){
+    console.log(i);
+}
+    
+    
+for(i=1; i<=6; i++){
+    console.log(i*i);
+    
+}
+
+    
+for(i=1; i<=50; i++){
+    if(i%5==0)
+console.log("count");
+}
+
+for(i =1; i <=5; i++)
+{
+    console.log("Hello");
+}
+
+for(i = 2; i <= 10; i = i + 2)
+{
+    console.log(i);
+}
+
+
+
+
+
+
