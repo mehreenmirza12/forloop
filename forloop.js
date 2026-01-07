066c31715604567ef1dbbef67a1d85b4e0f57084
mirzamehreen <mirzamehreen@gmail.com>
2026-01-07 08:25:08 -0800

    first commit

--- forloop.js
+++ forloop.js
@@ -29,7 +29,45 @@ for(i = 2; i <= 10; i = i + 2)
 {
     console.log(i);
 }
+let sum = 0;
+for(let i=1; i<=10; i ++){
+    sum + i;
 
+}
+console.log("sum", sum);
+
+function greet(){
+    console.log("Hello , welcom");
+    
+}
+greet();
+ function greetUser(name){
+    console.log("Hello" + name);
+    
+ }
+greetUser("mehreeen");
+greetUser("Ali");
+
+//add tow numbers
+
+function add (a,b){
+    console.log(a+b);
+    
+}
+add(5,5);
+add(20,30);
+
+//sum using function
+
+function calculatesum (limit){
+    let sum=0;
+    for (let i=1; i<=limit; i++){
+        sum =i;
+    }
+    return sum;
+
+}
+console.log(calculatesum(10));
 
 
 
---
+++ stringmethod.js
@@ -0,0 +1,83 @@
+let day = 3;
+switch(day) {
+    case 1:
+        console.log("monaday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+        default:
+            console.log("invaled day");
+     
+}
+let  operator = "+";
+let a = 3;
+let b = 5;
+
+switch (operator){
+
+
+case "+":
+    console.log(a+b);
+    break;
+ case "-":
+    console.log(a-b);
+    break;
+    case "*":
+        console.log(a*b);
+        break;
+    default:
+        console.log("unknown operator");
+            
+        
+       
+
+
+// toUpperCase()
+
+let name = "mehreen";
+console.log(name.toUpperCase());
+
+//includes()
+
+let sentence = "my name is";
+console.log(sentence.includes("mehreen"));
+
+toLowerCase()
+
+let nam = "MEHREEN";
+console.log(nam.toLowerCase());
+
+//slice()
+
+
+let phone = "03122015379";
+console.log(phone.slice(5));
+
+
+//CharAt()
+
+
+let text = "Hellow";
+console.log(text.charAt(2));
+
+
+//method chaning
+
+//trim()
+
+
+let input = " hellow ";
+
+let result = input.trim().toUpperCase();
+
+console.log(result);
+
+
+
+
+
+
