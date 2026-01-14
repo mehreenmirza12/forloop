5d4d68ad78cbcd92f1f5f01ce1cb0e93bb0eb243
mirzamehreen <mirzamehreen@gmail.com>
2026-01-14 08:15:36 -0800

    first commit

---
+++ Nfunction.js
@@ -0,0 +1,31 @@
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(5,6));
+
+function myName(name){
+
+console.log("mehreen");}
+
+myName();
+ 
+function squre(num){
+    return num*num;
+}
+console.log(squre(5));
+
+function addNumber (a,b){
+    return a+b;
+
+}
+console.log(addNumber(4,3));
+
+function isEven(num){
+    if(num%2===0){
+        return "Even number";
+    }
+    else{
+
+    return "odd number";}}
+console.log(isEven(7));
